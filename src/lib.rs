85c2deaa35d2e24fd89b183f2dcbd33b9bfc34d6
Your Name <you@example.com>
2026-03-04 18:52:03 +0100

    Revert "init"
    This reverts commit 7d32940d8b157031a8c5c3acdebb029310c1a988.

--- src/lib.rs
+++
@@ -1,5 +0,0 @@
-mod header;
-mod protocol;
-
-pub use header::{Header, HeaderError};
-pub use protocol::{MessageType, Message};
--- target/debug/deps/protocol-98f63a510c1d8edb.d
+++
@@ -1,7 +0,0 @@
-/home/proofofgenius/personal/programming/rust/exchanger/protocol/target/debug/deps/protocol-98f63a510c1d8edb.d: src/lib.rs src/header.rs src/protocol.rs
-
-/home/proofofgenius/personal/programming/rust/exchanger/protocol/target/debug/deps/libprotocol-98f63a510c1d8edb.rmeta: src/lib.rs src/header.rs src/protocol.rs
-
-src/lib.rs:
-src/header.rs:
-src/protocol.rs:
